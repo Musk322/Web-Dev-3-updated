f53110b88eeae8b465e98ef341d5321a43424b14
Musk322 <muski7806@gmail.com>
2026-09-21 13:58:13 +0530

    code update

--- Lecture18/server.js
+++ Lecture18/server.js
@@ -1,33 +1,38 @@
-const express = require ("express");
+const express = require("express");
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req,res) =>{
-    try{
+app.get('/', (req, res, next) => {
+    try {
         throw new Error("Something went wrong");
-    } catch(error){
-        res.status(500).json({sucess:false, message:"Something went wrong"});
+    } catch (error) {
+        //res.status(500).json({sucess:false, message:"Something went wrong"});
+        next(error);
     }
 })
- app.get("/:id", (req,res)=>{
+app.get("/:id", (req, res) => {
     const id = req.params.id;
-    try{
-        if(id==="1234"){
+    try {
+        if (id === "1234") {
             res.send("Valid id");
-        }else{
+        } else {
             throw new Error("Invalid id");
         }
-    }catch(error){
-        res.status(400).json({success:false, message: "Invalid id"});
+    } catch (error) {
+        //res.status(400).json({success:false, message: "Invalid id"});
+        next(error);
     }
-    
- });
 
- app.use((req,res)=>{  //Invalid route middleware
-    res.status(404).json({success:false, message:"Page not found"});
- })
+});
+app.use((err, req, res, next) => {  //error middelware
+    res.status(500).json({ success: false, message: "Page not found" });
+})
+
+app.use((req, res) => {  //Invalid route middelware
+    res.status(404).json({ success: false, message: err.message });
+})
 
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log(`server is running on port http://localhost:${PORT}`);
 })
